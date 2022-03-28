9d1138d1b62ee94fde3c59e214607406b8267cd6
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-03-28 16:07:43 +0200

    did the thingy

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -12,28 +12,30 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class ImageViewerWindowController
-{
+public class ImageViewerWindowController extends Thread {
     private final List<Image> images = new ArrayList<>();
     private int currentImageIndex = 0;
+    private boolean exit = false;
 
+    private int threadTime;
     @FXML
     Parent root;
 
     @FXML
     private ImageView imageView;
 
+    public ImageViewerWindowController() {
+    }
+
     @FXML
-    private void handleBtnLoadAction()
-    {
+    private void handleBtnLoadAction() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select image files");
         fileChooser.getExtensionFilters().add(new ExtensionFilter("Images",
                 "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp"));
         List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
 
-        if (!files.isEmpty())
-        {
+        if (!files.isEmpty()) {
             files.forEach((File f) ->
             {
                 images.add(new Image(f.toURI().toString()));
@@ -43,10 +45,8 @@ public class ImageViewerWindowController
     }
 
     @FXML
-    private void handleBtnPreviousAction()
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnPreviousAction() {
+        if (!images.isEmpty()) {
             currentImageIndex =
                     (currentImageIndex - 1 + images.size()) % images.size();
             displayImage();
@@ -54,20 +54,40 @@ public class ImageViewerWindowController
     }
 
     @FXML
-    private void handleBtnNextAction()
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnNextAction() {
+        if (!images.isEmpty()) {
             currentImageIndex = (currentImageIndex + 1) % images.size();
             displayImage();
         }
     }
 
-    private void displayImage()
-    {
-        if (!images.isEmpty())
-        {
+    private void displayImage() {
+        if (!images.isEmpty()) {
             imageView.setImage(images.get(currentImageIndex));
         }
+    }
+
+    public void handleStartSlideShow(ActionEvent actionEvent) throws InterruptedException {
+        {
+            start();
+        }
+    }
+
+    public void handleStopSlideShow(ActionEvent actionEvent) {
+        stop();
+    }
+
+//FIX ME ADD POP TO DECLARE SLEEP TIME, BETWEEN 1000-5000
+    @Override
+    public void run() {
+            while (!images.isEmpty()) {
+                handleBtnNextAction();
+                try {
+                    //Replace this with variable threadSleep
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
     }
 }
