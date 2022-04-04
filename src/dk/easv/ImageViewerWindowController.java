ad7ebd346f47f87c48119eca5a1013c3ac99740d
MathiasKrarup <math249e@easv365.dk>
2022-04-04 20:43:16 +0200

    Revert "bg"
    This reverts commit 2d9c1869f465749c66eb4b8e8c976aace1eaf848.

--- src/dk/easv/ImageViewerWindowController.java
+++ src/dk/easv/ImageViewerWindowController.java
@@ -1,50 +1,22 @@
 package dk.easv;
 
 import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-<<<<<<< Updated upstream
-=======
-import java.util.ResourceBundle;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
->>>>>>> Stashed changes
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-<<<<<<< Updated upstream
 import javafx.scene.control.TextField;
-=======
-import javafx.scene.control.*;
->>>>>>> Stashed changes
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class ImageViewerWindowController implements Initializable {
+import javax.swing.*;
 
-
-<<<<<<< Updated upstream
 public class ImageViewerWindowController extends Thread {
     private final List<Image> images = new ArrayList<>();
-=======
-    private final List<Image> images = new ArrayList<>();
-
-
-    public Slider durationSlider;
-    public Button btnPrevious;
-    public Button btnLoad;
-    public Button btnNext;
-    public Button stopBtn;
-    @FXML
-    private TextArea displayArea;
->>>>>>> Stashed changes
     private int currentImageIndex = 0;
     private boolean isStopped = false;
     @FXML
@@ -53,38 +25,13 @@ public class ImageViewerWindowController extends Thread {
     private int threadTime;
     @FXML
     Parent root;
+
     @FXML
     private ImageView imageView;
 
-<<<<<<< Updated upstream
-=======
-    public DisplayTask displayTask;
-
-    public int waitTime = 2;
-
-    ScheduledExecutorService executorService;
->>>>>>> Stashed changes
 
     public ImageViewerWindowController() {
     }
-
-<<<<<<< Updated upstream
-=======
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        stopBtn.setDisable(true);
-
-        durationSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            displayTime.setText(newValue.intValue() + "");
-            waitTime = newValue.intValue();
-            if (displayTask != null && displayTask.isRunning())
-                displayTask.setWaitTime(newValue.intValue());
-        });
-        durationSlider.setValue(3);
-    }
-
->>>>>>> Stashed changes
 
 
     @FXML
@@ -128,43 +75,13 @@ public class ImageViewerWindowController extends Thread {
     }
 
     public void handleStartSlideShow(ActionEvent actionEvent) throws InterruptedException {
-<<<<<<< Updated upstream
             start();
             isStopped = false;
 
         }
-=======
-        if (isStopped) {
-
-            displayTask = new DisplayTask(images, waitTime);
-            executorService = Executors.newScheduledThreadPool(1);
-
-            displayTask.valueProperty().addListener(((observable, oldValue, newValue) -> {
-                imageView.setImage(newValue);
-            });
-
-            displayArea.textProperty().bind(displayTask.messageProperty());
-
-            displayTask.setOnSucceeded(event -> {
-                displayImage();
-            });
-
-            Runnable slide = this::handleBtnNextAction;
-            calculateTime();
-            executorService.scheduleAtFixedRate(slide, calculateTime(), calculateTime(), TimeUnit.SECONDS);
-            isStopped = false;
-            System.out.println("start");
-        }
-    }
-
-    public Long calculateTime() {
-        return Long.parseLong(displayTime.getText());
-    }
->>>>>>> Stashed changes
 
     //FIX ME ADD POP TO DECLARE SLEEP TIME, BETWEEN 1000-5000
     public void handleStopSlideShow(ActionEvent actionEvent) throws InterruptedException {
-<<<<<<< Updated upstream
             isStopped = true;
             stop();
         }
@@ -183,11 +100,5 @@ public class ImageViewerWindowController extends Thread {
                     e.printStackTrace();
                 }
             }
-=======
-        if (!isStopped) {
-            executorService.shutdown();
-            isStopped = true;
-        }
->>>>>>> Stashed changes
     }
 }
